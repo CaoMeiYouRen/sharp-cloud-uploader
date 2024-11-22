84603a5719df7694733f813824f29f77fb21a864
CaoMeiYouRen <996881204@qq.com>
2024-11-23 02:17:09 +0800

    refactor(s3): move s3 client initialization to class constructor - 将 S3 客户端初始化移动到类构造函数中，以提高代码的可维护性和封装性

--- src/libs/s3.ts
+++ src/libs/s3.ts
@@ -2,15 +2,6 @@ import { S3Client, PutObjectCommand, PutObjectCommandInput } from '@aws-sdk/clie
 import { Storage } from './storage'
 import { getFileType } from '@/utils/file'
 
-const s3Client = new S3Client({
-    region: process.env.S3_REGION,
-    endpoint: process.env.S3_ENDPOINT || `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.S3_REGION}.amazonaws.com`,
-    credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY_ID,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    },
-})
-
 const S3_BASE_URL = process.env.S3_BASE_URL || `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.S3_REGION}.amazonaws.com`
 
 /**
@@ -18,6 +9,20 @@ const S3_BASE_URL = process.env.S3_BASE_URL || `https://${process.env.S3_BUCKET_
  * 需要设置 S3_BUCKET_NAME, S3_REGION, S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY，可选 S3_BASE_URL
  */
 export class S3Storage extends Storage {
+    private s3Client: S3Client
+
+    constructor() {
+        super()
+        this.s3Client = new S3Client({
+            region: process.env.S3_REGION,
+            endpoint: process.env.S3_ENDPOINT || `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.S3_REGION}.amazonaws.com`,
+            credentials: {
+                accessKeyId: process.env.S3_ACCESS_KEY_ID,
+                secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+            },
+        })
+    }
+
     async upload(buffer: Buffer, filename: string, contentType?: string): Promise<{ url: string }> {
         // S3 上传逻辑
         const params: PutObjectCommandInput = {
@@ -28,7 +33,7 @@ export class S3Storage extends Storage {
         }
 
         const command = new PutObjectCommand(params)
-        await s3Client.send(command)
+        await this.s3Client.send(command)
         const url = new URL(filename, S3_BASE_URL)
         return { url: url.toString() }
     }
